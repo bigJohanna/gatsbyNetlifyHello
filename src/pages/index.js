ae80072e9080a0715035492f60fba0b3b66d6f44
Johanna <svallingson@gmail.com>
2021-01-24 12:41:23 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{ color: `purple`, fontSize: `72px` }}>Hello everybody!
+    <h1>It's a wonderful day.</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
 }
